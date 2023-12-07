fb46b73ff30aa29592a3cd9d08ef26da578fb87e
Ryan Ptomey <rptomey6@gmail.com>
2023-12-07 12:39:19 -0600

    reconfigured part 1 and working part 2

--- day07_poker/aoc23_07.py
+++ day07_poker/aoc23_07.py
@@ -46,20 +46,73 @@ def card_to_int(card):
         return 10
     else:
         return int(card)
+    
+def check_hand_type_wild(cards):
+    frequencies = Counter(cards)
+    """
+    Every hand is exactly one type. From strongest to weakest, they are:
+
+    Five of a kind, where all five cards have the same label: AAAAA
+    Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+    Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+    Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+    Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+    One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+    High card, where all cards' labels are distinct: 23456
+    """
+    jack_count = frequencies["J"]
+    del frequencies["J"]
+    type_counts = frequencies.values()
+    
+    if jack_count == 0:
+        if 5 in type_counts:
+            return ["five of a kind", 7]
+        elif 4 in type_counts:
+            return ["four of a kind", 6]
+        elif 3 in type_counts and 2 in type_counts:
+            return ["full house", 5]
+        elif 3 in type_counts:
+            return ["three of a kind", 4]
+        elif Counter(type_counts)[2] == 2:
+            return ["two pair", 3]
+        elif 2 in type_counts:
+            return ["one pair", 2]
+        else:
+            return ["high card", 1]
+    else:
+        if jack_count == 5:
+            return ["five of a kind", 7]
+        elif max(type_counts) + jack_count == 5:
+            return ["five of a kind", 7]
+        elif max(type_counts) + jack_count == 4:
+            return ["four of a kind", 6]
+        elif Counter(type_counts)[2] == 2 and jack_count == 1:
+            return ["full house", 5]
+        elif max(type_counts) + jack_count == 3:
+            return ["three of a kind", 4]
+        elif Counter(type_counts)[2] == 1 and jack_count == 1:
+            return ["two pair", 3]
+        elif jack_count == 1:
+            return ["one pair", 2]
+        else:
+            return ["high card", 1]
+    
+def card_to_int_wild(card):
+    if card == "A":
+        return 14
+    elif card == "K":
+        return 13
+    elif card == "Q":
+        return 12
+    elif card == "J":
+        return 1
+    elif card == "T":
+        return 10
+    else:
+        return int(card)
 
 def parse(file_name):
     hands = []
-    hands_by_type = {
-        1:[],
-        2:[],
-        3:[],
-        4:[],
-        5:[],
-        6:[],
-        7:[]
-    }
-
-    sorted_hands_by_type = {}
 
     with open(file_name) as f:
         for line in f:
@@ -68,15 +121,27 @@ def parse(file_name):
                 values = re.findall(r"\w+", clean_line)
                 hand = {
                     "cards": values[0],
-                    "card_ints": [],
                     "bet": int(values[1])
                 }
-                for card in hand["cards"]:
-                    hand["card_ints"].append(card_to_int(card))
-                hand_type = check_hand_type(hand["cards"])
-                hand["type"] = hand_type[0]
-                hand["type_rank"] = hand_type[1]
-                hands_by_type[hand["type_rank"]].append(hand)
+                hands.append(hand)
+
+    return hands
+
+def part1(data):
+    hands = []
+
+    hands_by_type = {}
+    for i in range(7):
+        hands_by_type[i+1] = []
+
+    sorted_hands_by_type = {}
+
+    for hand in data:
+        hand["card_ints"] = [card_to_int(card) for card in hand["cards"]]
+        hand_type = check_hand_type(hand["cards"])
+        hand["type"] = hand_type[0]
+        hand["type_rank"] = hand_type[1]
+        hands_by_type[hand["type_rank"]].append(hand)
 
     for hand_type in hands_by_type.keys():
         sorted_hands_by_type[hand_type] = sorted(hands_by_type[hand_type], key = lambda x: (x["card_ints"][0], x["card_ints"][1], x["card_ints"][2], x["card_ints"][3], x["card_ints"][4]), reverse=True)
@@ -95,19 +160,52 @@ def parse(file_name):
     for hand_type in reversed(sorted_hands_by_type.keys()):
         hands.extend(sorted_hands_by_type[hand_type])
 
-    return hands
-
-def part1(data):
-    """Solve part 1."""
     winnings = 0
 
-    for hand in data:
+    for hand in hands:
         winnings += hand["bet"] * hand["global_rank"]
 
     return winnings
 
 def part2(data):
-    pass
+    hands = []
+
+    hands_by_type = {}
+    for i in range(7):
+        hands_by_type[i+1] = []
+
+    sorted_hands_by_type = {}
+
+    for hand in data:
+        hand["card_ints"] = [card_to_int_wild(card) for card in hand["cards"]]
+        hand_type = check_hand_type_wild(hand["cards"])
+        hand["type"] = hand_type[0]
+        hand["type_rank"] = hand_type[1]
+        hands_by_type[hand["type_rank"]].append(hand)
+
+    for hand_type in hands_by_type.keys():
+        sorted_hands_by_type[hand_type] = sorted(hands_by_type[hand_type], key = lambda x: (x["card_ints"][0], x["card_ints"][1], x["card_ints"][2], x["card_ints"][3], x["card_ints"][4]), reverse=True)
+
+    for hand_type in sorted_hands_by_type.keys():
+        preceding_hands = 0
+        if hand_type > 1:
+            for i in range(1,hand_type):
+                preceding_hands += len(sorted_hands_by_type[i])
+        number_in_type = len(sorted_hands_by_type[hand_type])
+        for hand_index in range(number_in_type):
+            sub_rank = number_in_type - hand_index
+            sorted_hands_by_type[hand_type][hand_index]["sub_rank"] = sub_rank
+            sorted_hands_by_type[hand_type][hand_index]["global_rank"] = sub_rank + preceding_hands
+
+    for hand_type in reversed(sorted_hands_by_type.keys()):
+        hands.extend(sorted_hands_by_type[hand_type])
+
+    winnings = 0
+
+    for hand in hands:
+        winnings += hand["bet"] * hand["global_rank"]
+
+    return winnings
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
