42303447ccd491b39aa2d380f805478b109793d5
Ryan Ptomey <rptomey6@gmail.com>
2023-12-11 14:45:34 -0600

    day 10 part 1 only

---
+++ day10_pipes/aoc23_10.py
@@ -0,0 +1,114 @@
+import sys
+import os
+import networkx as nx
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+import re
+import math
+
+pipe_key = {
+    "|": ["north", "south"],
+    "-": ["east", "west"],
+    "L": ["north", "east"],
+    "J": ["north", "west"],
+    "7": ["south", "west"],
+    "F": ["south", "east"]
+}
+
+opp = {
+    "north": "south",
+    "east": "west",
+    "south": "north",
+    "west": "east"
+}
+
+def get_neighbor(direction, pos):
+    coord = [int(n) for n in pos.split(",")]
+    match direction:
+        case "north":
+            coord[1] -= 1
+        case "south":
+            coord[1] += 1
+        case "east":
+            coord[0] += 1
+        case "west":
+            coord[0] -= 1
+    return f"{coord[0]},{coord[1]}"
+
+def parse(file_name):
+    data = {}
+    network = {}
+
+    with open(os.path.join(__location__, file_name)) as f:
+        y = 0
+        for line in f:
+            if line != "\n":
+                clean_line = line.strip()
+                x = 0
+                for char in clean_line:
+                    if char == "S" or char in pipe_key.keys():
+                        node = {
+                            "c": char,
+                            "pos": f"{x},{y}"
+                        }
+
+                        if char == "S":
+                            data["start"] = f"{x},{y}"
+                            node["open_to"]  = ["north", "south", "east","west"]
+                        else:
+                            node["open_to"] = pipe_key[char]
+
+                        network[f"{x},{y}"] = node
+
+                    x += 1
+                y += 1
+
+    # Check for valid neighbors
+    for pos in network.keys():
+        this_node = network[pos]
+        neighbors = []
+        for opening in this_node["open_to"]:
+            neighbor = get_neighbor(opening, pos)
+            if neighbor in network.keys():
+                neighbors.append(neighbor)
+        this_node["neighbors"] = neighbors
+                
+    data["network"] = network
+    return data
+
+def part1(data):
+    start = data["start"]
+    network = data["network"]
+
+    G = nx.Graph().to_directed()
+
+    for pos in network.keys():
+        for neighbor in network[pos]["neighbors"]:
+            G.add_edge(pos, neighbor)
+
+    max_path = 0
+    for neighbor in network[start]["neighbors"]:
+        for cycle in nx.all_simple_paths(G, source=neighbor, target=start):
+            if len(cycle) > max_path:
+                max_path = len(cycle)
+                data["max_cycle"] = cycle
+    
+    return max_path / 2
+
+def part2(data):
+    pass
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    solution1 = part1(puzzle_input)
+    solution2 = part2(puzzle_input)
+
+    return solution1, solution2
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = parse(path)
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
